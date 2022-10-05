cf45b8d1607aa3f4ea455c4885d323f1fb148213
daveads <the.daveads@gmail.com>
2022-10-05 12:45:45 +0100

    profile model

--- model/profile.py
+++ model/profile.py
@@ -17,14 +17,15 @@ def main():
             username_id VARCHAR(25),
             name VARCHAR(20),
             location VARCHAR(15),
+            height VARCHAR(10),
+            dating_status VARCHAR(30),
+            looking_for VARCHAR(30),
             hobbies VARCHAR(100),
             biography VARCHAR(150),
-            height VARCHAR(10),
-            looking_for VARCHAR(30),
-            dating_status VARCHAR(30),
+            
+            premium int,
             premium_day int,
-            profile_date VARCHAR(12),
-            premium int
+            profile_date VARCHAR(12)
             )''')
       #print("Table created")
 
@@ -47,14 +48,15 @@ def main():
                                                                   "00000000022331",
                                                                   "daveads", 
                                                                   "internet",
+                                                                  "9'10",
+                                                                  "single",
+                                                                  "a nice lady",
                                                                   "love tinkering and playing with computers",
                                                                   "nothing much yet",
-                                                                  "9'10",
-                                                                  "a nice lady",
-                                                                  "single",
-                                                                  1,
-                                                                  "2022-10-03",
-                                                                  0))
+                                                                   
+                                                                   0,
+                                                                   0,
+                                                                  "2022-10-03"))
       print("default values inputed")
       con.commit()
 
@@ -67,22 +69,23 @@ def main():
 
 if __name__ == '__main__':
   main()
-  
+
 class profile_data():
 
-  def __init__(self, username, username_id, name, location, hobbies, biography, height, looking_for, dating_status):
+  def __init__(self, username, username_id, name, location, height, dating_status, looking_for, hobbies, biography ):
 
     self.username = username
     self.username_id = username_id
     self.name = name
     self.location = location 
     #self.gender = gender #check
+    self.height = height
+    self.dating_status = dating_status
+    self.looking_for = looking_for
     self.hobbies = hobbies
     self.biography = biography
-    self.height = height
-    self.looking_for = looking_for
     #self.sexuality = Sexuality
-    self.dating_status = dating_status
+    
 
 
     #Defaults
@@ -90,22 +93,26 @@ class profile_data():
     self.premium_day = 0
     self.profile_date = datetime.utcnow().strftime("%d-%m-%Y")
 
-    cur.execute("INSERT INTO confession VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (
+    try: 
+      cur.execute("INSERT INTO profile_detail VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (
       self.username,
       self.username_id,
       self.name,
       self.location,
+      self.height,
+      self.dating_status,
+      self.looking_for,
       self.hobbies,
       self.biography,
-      self.height,
-      self.looking_for,
-      self.dating_status,
 
       self.premium,
       self.premium_day,
 
       self.profile_date
     ))
-    con.commit()
-    con.close()
+      con.commit()
+      con.close()
+
+    except sqlite3.Error as error:
+      print(f"unable to create add {self.username} into the database >> ", error)
 
