b7a71b9a724ef7e9c8466d2f741dd386cdd811a0
daveads <the.daveads@gmail.com>
2022-10-05 14:16:38 +0100

    core utilites

---
+++ core/json_data.py
@@ -0,0 +1,14 @@
+import json
+f = open('./config.json')
+data = json.load(f)
+
+
+class Datajson():
+
+    def __init__(self):
+
+        self.profile_channel = data.get('profile_channel') 
+        self.profile_commands_admin = data.get('profile_commands_admin')
+        self.male_channel = data.get('male_channel')
+        self.female_channel = data.get('female_channel')
+        self.others_channel = data.get('others_channel')
