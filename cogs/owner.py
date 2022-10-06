fe903b2d0a652405cd744f3614ae7c6bef22bab0
daveads <the.daveads@gmail.com>
2022-10-06 08:19:28 +0100

    owner data

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,12 +1,17 @@
 from discord.ext import commands
+from core import json_data
+
+
+data = json_data.Datajson()
 
 class Owner(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
-    def check_if_it_is_me(ctx):
-        return ctx.message.author.id == 840152379122384896
+    def check_if_it_is_me(ctx):   
+        owner_bool = ctx.message.author.id in data.owner
+        return owner_bool
 
     @commands.command()
     @commands.check(check_if_it_is_me)
