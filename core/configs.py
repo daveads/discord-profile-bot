c0d8ffe5adbcbf040b5d1bdff4e6a0ae4d355492
daveads <the.daveads@gmail.com>
2022-10-06 11:28:31 +0100

    added logging

---
+++ core/configs.py
@@ -0,0 +1,36 @@
+import json
+import logging
+from rich.logging import RichHandler
+
+#logging.basicConfig(format='%(levelname)s ::: %(message)s')
+logging.basicConfig(format='::: %(message)s', handlers=[RichHandler()])
+
+# json
+f = open('./config.json')
+data = json.load(f)
+
+class Datajson():
+
+    def __init__(self):
+
+        #channels
+        self.profile_channel = data.get('profile_channel') 
+        self.profile_commands_admin = data.get('profile_commands_admin')
+        self.male_channel = data.get('male_channel')
+        self.female_channel = data.get('female_channel')
+        self.others_channel = data.get('others_channel')
+
+        #owner id
+        self.owner = data.get('owners_id')
+
+        #roles
+        self.premium_role = data.get('premium_role_id')
+
+
+for i in data:
+
+    if data.get(i):
+        pass
+
+    else:
+        logging.warning(f"<<<{i.upper()}>>> CONFIG MISSING")
