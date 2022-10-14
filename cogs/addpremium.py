cca281bec28ef8f732013c9b042f91587ada7495
daveads <the.daveads@gmail.com>
2022-10-14 16:28:11 +0100

    premium role

--- cogs/addpremium.py
+++ cogs/addpremium.py
@@ -1,5 +1,10 @@
 import discord
 from discord.ext import commands
+from core import configs
+from core import premiumdate
+
+#json 
+data = configs.Datajson()
 
 class Addpremium(commands.Cog):
 
@@ -9,18 +14,50 @@ class Addpremium(commands.Cog):
     #premium_p
     @commands.command()
     @commands.has_any_role('admin', 'moderator')
-    async def addbumpp(self, ctx, user: discord.Member, role: discord.Role):
-        #print(role.id)
-        if ctx.author.id == 840152379122384896:
-            await user.add_roles(role)
-            await ctx.send(f"{user.name} has been giving a role called: {role.name}")
-        else:
-            if role.name == "premium_p":
-                await user.add_roles(role)
-                #await ctx.send("user is now a premium user")
-                await ctx.send(f"{user.name} has been giving {role.name} role for profile bot access")
+    async def addbumpp(self, ctx, user: discord.Member, role = "premium_p", month=1):
+        roleObj = discord.utils.get(ctx.guild.roles, name=role)
+
+        if True: ## admin command channel
+            if True: ## if user has a profile
+                if roleObj in user.roles:
+                    #print(role.id)
+
+                    await ctx.send("user already have this role")
+
+                else:
+                    if role == "premium_p":
+                        #print(user.id)
+                        await user.add_roles(roleObj)
+                        await ctx.send(f"{user.name} has been giving {roleObj.name} access")
+                    else:
+                        await ctx.send("You are only allowed to give **premium_p** role with this bot")
             else:
-                await ctx.send("You are only allowed to give **premium_p** role with this bot")
+                await ctx.send("user doesn't have a profile")
+        else:
+            await ctx.send("command has to be run in the adminCommand channel")
+
+
+    @commands.command()
+    @commands.has_role('admin')
+    async def mute(self, ctx, user: discord.Member, role="premium_p"):
+
+        roleObj = discord.utils.get(ctx.guild.roles, name=role)
+        if True: #admin command channel
+            if True: #if user has role and user has a profile
+
+                if roleObj in user.roles:
+
+                    if role == "premium_p":
+                        await user.remove_roles(roleObj)
+                        await ctx.send("{} has been muted".format(user))
+
+                    else:
+                        await ctx.send("You are only allowed to give **premium_p** role with this bot")
+                else:
+                    await ctx.send("you doesn't have the role")
+
+        else:
+            await ctx.send("command can only be used in the admin channel")
 
 
     @addbumpp.error
