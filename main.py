bb576813b3e31fadfd51936c58a359bc0fc99cef
daveads <the.daveads@gmail.com>
2023-01-02 17:07:35 +0100

    bot intro msg

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ async def prep(ctx: commands.Context):
     await ctx.message.channel.purge(limit=10)
     await ctx.send(file=discord.File(bot_configs.profile_image()))
 
-    info = """**Create your own profile to describe yourself and start meeting others!** \n\n  📝 ➤ Edit Profile \n  `Update your information` \n\n 🔎 ➤ Preview Profile \n  `Preview your current profile!` \n\n 📢 ➤ Bump the server \n  `cooldown: (Normal 3d)` 
+    info = """** *Required Roles to create a profile*\n`Gender, Age, Orientation, Dating-status, Dm-status, Height` \n\nCreate your own profile to describe yourself and start meeting others!** \n\n  📝 ➤ Edit Profile \n  `Update your information` \n\n 🔎 ➤ Preview Profile \n  `Preview your current profile!` \n\n 📢 ➤ Bump the server \n  `cooldown: (Normal 3d)` 
            \n **For premium user should use the `/bumpp` command *~~Coming soon~~* ** \n *The `/bumpp command cost $3 monthly` message moderators to get more details on that* \n  `cooldown: (Normal: 12hrs)` 
             \n """
 
