091b08225c62c1159687b3aef4f00025e447525c
daveads <the.daveads@gmail.com>
2023-01-25 14:11:46 +0100

    fix

--- view/announce_modal.py
+++ view/announce_modal.py
@@ -1,23 +1,23 @@
 import discord
-
+import asyncio
 from core.config_parser import BotConfigs
 botconfig = BotConfigs()
 
-class Announce(discord.ui.Modal):
+class Announce(discord.ui.Modal, title="Profile Bot"):
 
 
-    title = discord.ui.TextInput(
+    Title = discord.ui.TextInput(
         label='Title',
         style=discord.TextStyle.short,
-        required=False,
+        required=True,
         max_length=100,
     )
 
 
     announcement = discord.ui.TextInput(
         label='Announcement',
-        style=discord.TextStyle.short,
-        required=False,
+        style=discord.TextStyle.long,
+        required=True,
         max_length=300,
     )
 
@@ -25,10 +25,12 @@ class Announce(discord.ui.Modal):
 
         announce_chn = discord.utils.get(interaction.guild.channels, id=botconfig.channel("announcement_chn"))
 
-        embed = discord.Embed(title="Profile bot feature annoucement", description=f"Title : `{self.title.value}` " , color=discord.Colour(0x2f3136),)
+        embed = discord.Embed(title="Profile bot feature annoucement", description=f"Title : `{self.Title.value}` " , color=discord.Colour(0x2f3136),)
 
         embed.add_field(name="INFO", value=self.announcement.value)
         embed.set_footer(text="From @daveads")
 
         await announce_chn.send(embed=embed)
-        await interaction.response.send_message("**Announcement Sent!**", delete_after=5)
+        await interaction.response.send_message("**Announcement Sent!**", ephemeral=True)
+        await asyncio.sleep(20)
+        await interaction.delete_original_response()
