01e3ae36c04966481d94c957fd783ef9b91f4c5a
daveads <the.daveads@gmail.com>
2023-02-10 04:39:29 +0100

    same profile data

--- cogs/search_user.py
+++ cogs/search_user.py
@@ -1,10 +1,9 @@
 from typing import Optional
 from discord.ext import commands
 import discord
-import asyncio
 from discord import app_commands 
 from model import queries
-
+from core.profile_data import profile_embed
 
 user_in_db = queries.PROFILEque()
 
@@ -12,8 +11,7 @@ class search(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.dic_key = ['id','username','username_id','name','location','looking_for','hobbies','biography','premium_day','profile_date']
-    pass
+    
     # /slash command
 
 
@@ -25,38 +23,20 @@ class search(commands.Cog):
 
 
     @app_commands.checks.dynamic_cooldown(cooldown_for_everyone_but_me)
-    @app_commands.describe(first_value='Input user id',)
+    @app_commands.describe(userid='Input user id',)
     @app_commands.command(name="find", description="search for a user profile")
-    async def find(self, interaction: discord.Interaction, first_value : str):
+    async def find(self, interaction: discord.Interaction, userid : str):
         """ /find """
 
         try:
-            convert_id = int(first_value)
+            convert_id = int(userid)
         
             user = await self.bot.fetch_user(convert_id)
 
             if user_in_db.get_user(user.id):
-                user_d = user_in_db.get_user(user.id)
-                user_data ={}
-          
-                for i in range(len(self.dic_key)):
-                    user_data[self.dic_key[i]] = user_d[i]
-
-                #await interaction.response.send_message(f'your data {user_data}, {interaction.user.mention} {first_value}' , ephemeral=False)
-
-
-                # Emded
-
-                embed=discord.Embed(title=f"PROFILE `{user_data['username']}` ", color=discord.Color.blue())
-                embed.set_thumbnail(url=user.avatar)
-                embed.add_field(name="NAME ", value=f"{user_data['name']}", inline=True)
-                embed.add_field(name="LOCATION", value=f"{user_data['location']}", inline=True)
-                embed.add_field(name="LOOKING FOR", value=f"{user_data['looking_for']}", inline=True)
-                embed.add_field(name="HOBBIES", value=f"{user_data['hobbies']}",inline=True)
-                embed.add_field(name="BIOGRAPHY", value=f"{user_data['biography']}", inline=True)
-                embed.set_footer(icon_url=user.avatar)
-
-                await interaction.response.send_message(embed=embed)
+              
+                profile_embed_data = await profile_embed(user, interaction)
+                await interaction.response.send_message(embed=profile_embed_data)
 
 
             else: 
@@ -76,3 +56,7 @@ class search(commands.Cog):
 
 async def setup(bot):
     await bot.add_cog(search(bot))
+
+
+
+# age, verified level, gender 
