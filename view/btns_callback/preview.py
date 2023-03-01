5cbf082e263ba8613d894a7db53221d1b70a9b58
daveads <the.daveads@gmail.com>
2023-03-01 11:51:55 +0100

    refractory

--- view/btns_callback/preview.py
+++ view/btns_callback/preview.py
@@ -3,41 +3,29 @@ import discord
 from core import embed
 from model import queries
 
+from core.profile_data import profile_embed
+
 # Class initialized
 user_in_db = queries.PROFILEque()
 user_embed = embed.Embed()
 
 async def preview(bot, interaction, button):
-    dic_key = ['id','username','username_id','name','location','looking_for','hobbies','biography','premium_day','profile_date']
-
+    
     user = await bot.fetch_user(interaction.user.id)
 
     if user_in_db.get_user(interaction.user.id):
-        
-        user_d = user_in_db.get_user(interaction.user.id)
-        user_data ={}
-        for i in range(len(dic_key)):
-            user_data[dic_key[i]] = user_d[i]
 
-           
-        embed=discord.Embed(title=f"PROFILE `{user_data['username']}` ", description="Your current profile details", color=discord.Color.blue())
-        embed.set_thumbnail(url=user.avatar)
-        embed.add_field(name="NAME ", value=f"{user_data['name']}", inline=True)
-        embed.add_field(name="LOCATION", value=f"{user_data['location']}", inline=True)
-        embed.add_field(name="LOOKING FOR", value=f"{user_data['looking_for']}", inline=True)
-        embed.add_field(name="HOBBIES", value=f"{user_data['hobbies']}",inline=True)
-        embed.add_field(name="BIOGRAPHY", value=f"{user_data['biography']}", inline=True)
-        embed.set_footer(text="profile can only be edited once a week",icon_url=user.avatar)
+        profile_embed_data = await profile_embed(user, interaction)
+
 
         try:
-            await user.send(embed=embed)
+            await user.send(embed=profile_embed_data)
             await interaction.response.defer()
             
         except:
-            await interaction.response.send_message(embed=embed, ephemeral=True)
+            await interaction.response.send_message(embed=profile_embed_data, ephemeral=True)
             await asyncio.sleep(60)
             await interaction.delete_original_response()
-
 
 
 
